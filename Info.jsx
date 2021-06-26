edfc19275b015d091874033c2ab85be1e10d2ded
Niran1569 <81973153+Niran1569@users.noreply.github.com>
2021-06-26 19:04:36 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div class="note">
+      <h1>Javascript and React</h1>
+      <p>A basic Web development React Js Bootcamp</p>
+    </div>
+  );
+}
+export default Info;
